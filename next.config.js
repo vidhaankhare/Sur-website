e0bd97e053c30e4c5f29accbefb6499dd8b37dc1
“[firstname <vidhankhare16@gmail.comgit config --global user.name Vidhaankhare16>
2025-08-13 16:57:04 +0530

    Update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Enable React DevTools in development
-  reactDevOverlay: true,
   
   // TypeScript configuration
   typescript: {
